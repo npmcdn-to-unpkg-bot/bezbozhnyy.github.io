d5b5ce07954d2a5ce56b9a31b7b5422c691602c2
Vladimir Bezbozhnyy <vladimir.bezbozshniy@beetroot.se>
2016-04-26 02:08:13 +0300

    edit homeTAsk 1-2

--- js2/js/script.js
+++ js2/js/script.js
@@ -1,18 +1,22 @@
-var arr = [];
-for (var i = 0; i < 5; i++){
-    arr.push(prompt('Привет! Введите имена:',''));
-};
+	var arrNames = [];
 
-var flag;
-var userName = prompt('Введите свое имя (Enter your User Name)','');
-for (i = 0; i < arr.length; i++){
-    if (arr[i] === userName){
-        flag = true;
-    }
-};
-if (flag == true && userName != null) {
-    alert(userName + ', добро пожаловать!!!' );
-} else
-    {
-        alert('Sorry, but username' + userName + ' not found :(((')
-    };
+	for (var i = 0; i < 5; i++ ){
+		arrNames[i] = prompt('Enter any name');
+		console.log('arrNames =',arrNames);
+	}
+
+	var login = prompt('Enter your username','name');
+	var flag = false;
+	console.log('login =',login);
+
+	for (var i = 0; i < arrNames.length; i++){
+		if (login === arrNames[i]) {
+			flag = true;
+		}
+	}
+
+	if (flag){
+		alert(login + ', you have successfully entered!!!')
+	} else {
+		alert('Unfortunately, you could not enter :-(((');
+	}
