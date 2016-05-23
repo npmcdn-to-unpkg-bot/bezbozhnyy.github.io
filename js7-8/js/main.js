6eb123373227afd456b3db389a1664421446b57f
bezbozhnyy <bezbozhnyy@gmail.com>
2016-05-24 02:15:21 +0300

    added homeTask JS 7-8

---
+++ js7-8/js/main.js
@@ -0,0 +1,25 @@
+﻿$(function(){
+		'use strict'
+
+	$('.tabsInclude .tabsItem').not(':first').hide();
+
+	$('.wrapperMenu .tabsTitle').click(function(e){
+	e.preventDefault();
+
+	$('.wrapperMenu .tabsTitle').removeClass('selected').eq($(this).index()).addClass('selected');
+	$('.tabsInclude .tabsItem').hide().eq($(this).index()).fadeIn();
+	}).eq(0).addClass('selected');
+
+
+	$('form div.screentip_box').hover(
+		function(){
+		$(this).find('span').stop().animate({opacity: 1});
+		},
+		function(){
+		$(this).find('span').stop().animate({opacity: 0});
+		}),
+
+	$('div input.button').click(function() {
+		$('form span').stop().animate({opacity: 1});
+	});
+})
