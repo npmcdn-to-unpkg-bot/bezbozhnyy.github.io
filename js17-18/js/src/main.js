c45102189bbe396634ab6060d3e6fab67c5070db
Vladimir Bezbozhnyy <vladimir.bezbozshniy@beetroot.se>
2016-06-20 00:42:28 +0300

    edited bags 17-18

--- js17-18/js/src/main.js
+++ js17-18/js/src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function () {
   $('.carousel').carousel();
   var html = $('.user-profile').html();
@@ -26,33 +25,4 @@ $(function () {
   var contentLow = template(userData);
   body.append(contentLow);
 
-=======
-$(function () {
-  $('.carousel').carousel();
-  var html = $('.user-profile').html();
-  var body = $('body');
-
-  var userData = {
-      lfname: 'Безбожный Владимир Леонидович',
-      photo: 'img/myphoto.jpg',
-      prevJob: 'Преподаватель ВУЗа',
-      goal_cources: 'Хочу изучать Front-end потому что:',
-      whyDo1: 'Это чертовски интересно и увлекательно',
-      whyDo2: 'Здесь не "заржавеешь", новые идеи и задачи',
-      whyDo3: 'Это перспектино и благодарно',
-      tel: '+380661885001',
-      fb: 'https://www.facebook.com/bezbozhnyy',
-      feadback: 'Таки могёмЪ :-)'
-    };
-/*Simple JavaScript Templating John Resig*/
-  var content = tmpl(html, userData);
-
-  body.append(content);
-
-/*-----LoDash-----*/
-  var template = _.template('<p class="bonus">LoDash (bonus)</p>' + html);
-  var contentLow = template(userData);
-  body.append(contentLow);
-
->>>>>>> 5fa02b1f42ab2a0cf9621c1c0479013e41961764
 });
